2cb31e9da43b18ab6c0b01ab48e9bd6643e014f3
Xavier San Sebastian <xavi.sansebastian@gmail.com>
2024-03-19 21:12:22 +0100

    final commit. Exercise done!

--- sap_challenges/cloud_events/week3/triggerAwesomeEvent.js
+++ sap_challenges/cloud_events/week3/triggerAwesomeEvent.js
@@ -1,5 +1,4 @@
 const { CloudEvent, HTTP } = require("cloudevents");
-//const fetch = require('node-fetch');
 require('dotenv').config();
 
 //cloud event data
@@ -27,7 +26,7 @@ const soEvent = new CloudEvent({type,source,subject,data,sapcommunityid})
 
 //sending event to Solace
 const solace_protocol = "https"
-const solace_topic = `/dev-challenge/week-3/${sapcommunityid}/ce`
+const solace_topic = `dev-challenge/week-3/${sapcommunityid}/ce`
 
 const { headers, body } = HTTP.structured(soEvent);
 
@@ -38,14 +37,38 @@ const basicAuth = 'Basic ' + Buffer.from(process.env.SOLACE_USER + ':' + process
 
 headers["Authorization"] = basicAuth;
 
-//print the log
-
-
-console.log("### March Developer Challenge - CloudEvents: Week 2 ###\n");
-console.log("- HTTP Event Headers - \n")
+console.log("### March Developer Challenge - CloudEvents: Week 3 ###\n");
+console.log("- HTTP Request Event Headers - \n")
 for (const key in headers) {
     console.log(`Key: "${key}", Value: "${headers[key]}"`);
   }
 
-console.log("\n - HTTP Event Body - \n");
+console.log("\n - HTTP Request Event Body - \n");
 console.log(JSON.stringify(JSON.parse(body), null, 1) + "\n");
+console.log("\n - HTTP Response - ")
+
+// Sending POST request to Solace
+fetch(postURL, {
+    method: 'POST',
+    headers: headers,
+    body: body
+})
+.then(response => {
+    // Check if the response is OK
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    // Return a response with HTTP status and a message
+    return {
+        status: response.status,
+        message: 'CloudEvent successfully sent to Solace.'
+    };
+})
+.then(responseObj => {
+    // Print the response
+    console.log('Response:', responseObj);
+})
+.catch(error => {
+    // Print error message
+    console.error('There was a problem with the fetch operation:', error);
+});
